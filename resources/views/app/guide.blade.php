cee490d4483758887444551dd8a0ec1aaa9c9d75
SerhiiAndrosov <serhii@unifyordering.com>
2022-04-05 18:50:20 +0300

    New Guide page

--- resources/views/app/guide.blade.php
+++ resources/views/app/guide.blade.php
@@ -1,17 +1,13 @@
-@extends('app.layout')
+@extends('app.app_layout')
 @section('pageName')
 Guide
 @endsection
 @section('content')
-    <div class="row">
-        <div class="panel">
-            <div class="panel-heading">
-                <div class="panel-options pull-right">
-                </div>
-                <i class="fas fa-file-import"></i><h3 class="panel-title">A quick setup guide</h3>
-            </div>
+    <div class="links-table-wrap">
+        <div class="table-header">
             <div class="panel-body">
-                <h1>A quick setup guide</h1>
+                <h5>A quick setup guide</h5>
+                <p></p>
                 <iframe width="560" height="315" src="https://www.youtube.com/embed/cPSlEiaoHuM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
         </div>
