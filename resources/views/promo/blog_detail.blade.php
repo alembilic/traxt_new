2eb2ff39637e40e3e18668f2c0df6d9f58e51cc8
Torben Rohde <torben@rohdes.net>
2022-04-18 22:13:39 +0200

    blog html, css

--- resources/views/promo/blog_detail.blade.php
+++ resources/views/promo/blog_detail.blade.php
@@ -13,14 +13,36 @@
 @section('content')
 <section class="">
     <div class="container">
-        <div class="">
-            <h2 class="">{{ $post->getTitle() }}</h2>
-            <p class="">{{ $post->getCreatedAt()->format('M j, Y') }}</p>
+        <div class="row align-items-center pad-top-3 pad-bot-1 padding-sm-mob padding-lg-mob-top">
+            <div class=col-md-12>
+                <img src="/img/blog/blog_shape.svg" alt="" title="" class="blog-details-shape-left">
+                <p class="font-weight-bold text-primary text-center">Published {{ $post->getCreatedAt()->format('M j, Y') }}</p>
+                <h1 class="">{{ $post->getTitle() }}</h1>
+            </div>
+        </div>
+        <div class="row align-items-center pad-top-3 pad-bot-1 padding-sm-mob padding-lg-mob-top">
+            <div class=col-md-12>
+                <div class="img-wrap">
+                    <img src="/img/blog/blog_details.svg" alt="" title="" class="w-100">
+                </div>
+            </div>
+        </div>        
+        <div class="row pad-top-3 pad-bot-1 padding-lg-mob">
+            <div class="col-md-4 align-top">
+                <h3 class="text-primary text-left">
+                    Summary
+                </h3>
+                <p>
+                    {{ $post->getShortContent() }}
+                </p>
+            </div>
+            <div class="col-md-8">
+                {{ $post->getFullContent() }}
 
-            <p>
-                {!! $post->getFullContent() !!}
-            </p>
+                <p><a class="btn-custom bc-btn-primary" href="#">Share</a></p>
+            </div>
         </div>
     </div>
 </section>
 @endsection
+
--- resources/views/promo/blog_index.blade.php
+++ resources/views/promo/blog_index.blade.php
@@ -14,41 +14,46 @@
 @section('content')
 
 <section class="">
-    <div class="container">
-
+  <div class="container">
     @if ($mainPost)
-        <div class="jumbotron p-3 p-md-5 rounded">
-            <div class="col-md-6 px-0">
-            <h1 class="display-4 font-italic">{{ $mainPost->getTitle() }}</h1>
-            <p class="lead my-3">{!! $mainPost->getShortContent() !!}</p>
-            <p class="lead mb-0"><a href="/blog/{{ $mainPost->getUrlCode() }}" class="font-weight-bold">Continue reading...</a></p>
-            </div>
-        </div>
-    @endif
-
-    <div class="row mb-2">
-        @foreach($posts as $post)
+      <div class="row align-items-center pad-top-6 pad-bot-6 reverse-col-mob padding-lg-mob">
         <div class="col-md-6">
-          <div class="card flex-md-row mb-4 box-shadow h-md-250">
-            <div class="card-body d-flex flex-column align-items-start">
-              <!--strong class="d-inline-block mb-2 text-primary">World</strong>-->
-              <h3 class="mb-0">
-                <a class="text-dark" href="/blog/{{ $post->getUrlCode() }}">{{ $post->getTitle() }}</a>
-              </h3>
-              <div class="mb-1 text-muted">{{ $post->getCreatedAt()->format('M j, Y') }}</div>
-              <p class="card-text mb-auto">{!! $post->getShortContent() !!}</p>
-              <a href="/blog/{{ $post->getUrlCode() }}">Continue reading</a>
-            </div>
-            <img class="card-img-right flex-auto d-none d-md-block" data-src="holder.js/200x250?theme=thumb" alt="Thumbnail [200x250]" style="width: 200px; height: 250px;" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22200%22%20height%3D%22250%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20200%20250%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_18021f42a35%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A13pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_18021f42a35%22%3E%3Crect%20width%3D%22200%22%20height%3D%22250%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2256.20000076293945%22%20y%3D%22131%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true">
+          <div class="text-content">
+            <p class="font-weight-bold text-primary">{{ $mainPost->getCreatedAt()->format('M j, Y') }}</p>
+            <h1 class="text-left">{{ $mainPost->getTitle() }}</h1>
+            <p>
+              {!! $mainPost->getShortContent() !!}
+            </p>
+          </div>
+          <a class="btn-custom bc-btn-primary" href="/blog/{{ $mainPost->getUrlCode() }}">Read more</a>
+        </div>
+        <div class="col-md-6 image-element padding-sm-mob">
+          <img src="/img/blog/blog_shape.svg" alt="" title="" class="blog-shape-right">
+          <div class="img-wrap">
+            <img src="/img/blog/blog_jumbotron.svg" alt="" title="" class="w-100">
           </div>
         </div>
-        @endforeach
-
-    </div>
-
-    </div>
-</section>
+      </div>
+    @endif
     
+    <div class="card-deck">
+    @foreach($posts as $post)
+      <div class="col-md-4 mb-4">
+        <div class="card border-0">
+          <img class="card-img-top w-100" src="/img/blog/blog_post.svg" alt="blogpost" title="image-{{ $post->getTitle() }}">
+          <div class="card-body">
+            <p class="font-weight-bold text-primary">{{ $post->getCreatedAt()->format('M j, Y') }}</p>
+            <h3 class="card-title text-left">{{ $post->getTitle() }}</h5>
+            <p class="card-text">{{ $post->getShortContent() }}</p>
+            <p>
+              <a href="/blog/{{ $post->getUrlCode() }}">Read more</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    @endforeach
+    </div>
+  </div>
+</section>
+
 @endsection
-
-
