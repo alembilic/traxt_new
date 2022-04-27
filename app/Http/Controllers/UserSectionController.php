7ff424041281c27896c63f4e3672566269e06403
Torben Rohde <torben@rohdes.net>
2022-04-27 15:31:47 +0200

    remove test code

--- app/Http/Controllers/UserSectionController.php
+++ app/Http/Controllers/UserSectionController.php
@@ -175,21 +175,13 @@ class UserSectionController extends BaseWebController
             // search for email
             $criteria->where(Criteria::expr()->eq(Contact::EMAIL, $search));
             // search for domains
-            $criteria->orWhere(Criteria::expr()->contains(Contact::DOMAINS, ','.$search.','));
-            $criteria->orWhere(Criteria::expr()->contains(Contact::DOMAINS, $search.','));
-            $criteria->orWhere(Criteria::expr()->contains(Contact::DOMAINS, ','.$search));
-            $criteria->orWhere(Criteria::expr()->eq(Contact::DOMAINS, $search));
+            $criteria->orWhere(Criteria::expr()->contains(Contact::DOMAINS, $search));
     
-            //$query->where($query->expr()->andX(...$criteria));
             //$query->Where($query->expr()->eq('c.'.Contact::EMAIL, ':email'));
-            //$query->OrWhere("'rohdes.net' in c.domains) = true");
-            //$query->OrWhere("FIND_IN_SET('rohdes.net',c.domains) <> 0");
             //$query->setParameter('email', $search);
-            //$query->setParameter('domains', $search);
         }
 
         $contacts = collect($repository->matching($criteria));
-        
         //$contacts = $query->getQuery()->execute();
 
         return view('app.contacts', [
