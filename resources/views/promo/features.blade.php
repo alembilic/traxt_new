fa40b4076ec0b3bfaa93161dae37666a39a59b13
kristoffer <kristofferthetolboll@gmail.com>
2022-05-16 13:23:14 +0200

    added more shitg

--- resources/views/promo/features.blade.php
+++ resources/views/promo/features.blade.php
@@ -16,17 +16,17 @@
                     <div class="text-content">
                         <h1 class="pt-2 align-left">Get links that work and provide value.</h1>
                         <p class="pt-3 align-left ">
-                            Links are still the number 1 ranking factor, when it comes to organic positions in the Googles Search Results.
+                            Links are still the number one ranking factor for organic positions in search engine results.
                         </p>
                         <p class="pt-3 align-left ">
-                            Links that are not indexed or tagged wrong are not good for your SEO. Traxr gives the overview of all your links.
+                            If you are not indexed or tagged correctly, your SEO suffers. Traxr gives the overview of all your links, including those not working and why.
                         </p>
                     </div>
-                    <a class="btn-custom bc-btn-primary" href="/pricing">Sign up for Free now - No Creditcard needed</a>
+                    <a class="btn-custom bc-btn-primary" href="/pricing">Sign up for free now - no credit card needed</a>
                 </div>
                 <div class="col-md-6 image-element padding-sm-mob">
                     <div class="img-wrap">
-                        <img src="/img/vectors/features-vector-1.svg" alt="" title="" class="w-100">
+                        <img src="/img/vectors/features-vector-1.svg" alt="feature-vector-traxr-graph" class="w-100">
                     </div>
                 </div>
             </div>
@@ -38,20 +38,22 @@
             <div class="row align-items-center pad-top-6 pad-bot-6 padding-lg-mob">
                 <div class="col-md-6 image-element padding-sm-mob">
                     <div class="img-wrap">
-                        <img src="/img/vectors/features-vector-2.svg" alt="" title="" class="w-100">
+                        <img src="/img/vectors/features-vector-2.svg" alt="feature-vector-traxr-graph" class="w-100">
                     </div>
                 </div>
                 <div class="col-md-6">
                     <div class="text-content">
-                        <h2 class="pt-2 align-left">Manual precision</h2>
+                        <h2 class="pt-2 align-left">
+                            Proven backlink precision
+                        </h2>
                         <p class="pt-3 align-left ">
-                            100% link precision is the best way. We do not use predictive methods and substandard API’s to give us 60-80% accuracy.
+                            100% link precision is the best way to get value from your links. Our unique algorithm creates pinpoint accuracy that does not rely on external APIs and systems.
                         </p>
                         <p class="pt-3 align-left ">
-                            We let you take the full control and makes sure to only monitor what you deem important.
+                            We let you take complete control and only monitor what you deem important.
                         </p>
                     </div>
-                    <a class="btn-custom bc-btn-primary" href="/pricing">Sign up for Free now</a>
+                    <a class="btn-custom bc-btn-primary" href="/pricing">Sign up for free now</a>
                 </div>
             </div>
         </div>
@@ -64,13 +66,13 @@
                     <div class="text-content">
                         <h2 class="pt-2 align-left">24/7 monitoring</h2>
                         <p class="pt-3 align-left ">
-                            We will monitor your links 24/7 and make sure that you only get relevant notifications when changes to your link happens.
+                            We will monitor your links 24/7 and ensure that you only get relevant notifications when changes to your link happen. Our system provides a complete overview with <b>domain ranking</b>, <b>toxicity score</b>, <b>financial data <i>(profits and losses)</i></b>, <b>and much more...</b>.
                         </p>
                         <p class="pt-3 align-left ">
-                            100% accuracy is what we won’t to give you. Not false positives or estimated data. Real Facts
+
                         </p>
                     </div>
-                    <a class="btn-custom bc-btn-primary" href="/pricing">Sign up for Free now</a>
+                    <a class="btn-custom bc-btn-primary" href="/pricing">Sign up for free now</a>
                 </div>
                 <div class="col-md-6 image-element padding-sm-mob">
                     <div class="img-wrap">
@@ -88,9 +90,25 @@
                 <div class="col-md-12">
                     <div class="text-content">
                         <img src="/img/shapes/right-small-blue.svg" alt="" title="" class="home-shape-right">
-                        <h2 class="pt-2 align-center">The only tool you will ever need to Monitor your Links</h2>
+                        <h2 class="pt-2 align-center">Add backlinks any way you want</h2>
                         <p class="pt-3 align-center text-md-light text-cen-wid-70">
-                            <span class="fw-700">Traxr is an all in one solution to monitor the status and value of your links. Just look at these features</span></p>
+                            <span class="fw-700">Traxr has a bunch of features and import strategies you can read about below.</span></p>
+                        <h3>Import strategies:</h3>
+                        <br />
+                        <ul class="list-group">
+                            <li class="list-group-item">Import backlinks via CSV or Spreadsheet <img src="/img/icons/spreadsheet.svg" style="float: right" height="70" width="70"/>
+                            <p class="pt-3">Any spreadsheet or CSV file can be imported and directly used in Traxr</p>
+                                </li>
+
+                            <br />
+                            <li class="list-group-item">Import automatically by using our algorithms <img src="/img/icons/algo.svg" style="float: right" height="70" width="70"/>
+                            <p class="pt-3">Our state-of-the-art algorithms automatically finds all backlinks (active and not active), and allows you to import them directly in Traxr</p>
+                            </li>
+                            <br />
+                            <li class="list-group-item">Add manually <img src="/img/icons/manuel.svg" style="float: right" height="70" width="70"/>
+                            <p class="pt-3">Using your user interface you can add your backlinks by just typing in the URL, one by one, or adding multiple at the same time</p></li>
+
+                        </ul>
                     </div>
                 </div>
             </div>
@@ -103,16 +121,17 @@
                 <div class="col-md-4 padding-sm-mob">
                     <div class="features-box-2 pad-10">
                         <div class="text-content">
-                            <img src="/img/icons/features-icon-1.svg" alt="" title="" class="feature-icon">
-                            <h3 class="pt-2 align-left">Index Status</h3>
+                            <img src="/img/icons/features-icon-1.svg" alt="feature-icon" title="" class="feature-icon">
+                            <h3 class="pt-2 align-left">Index Status </h3>
                             <p class="pt-3 align-left ">
-                                Each url linking to your site is checked to see if its indexed in Google.
+                                A backlink is worthless if Google doesn't know about it.
                             </p>
                             <div class="features-bottom-text-wrapper">
                                 <div class="list-icon"></div>
                                 <p class="pt-3 align-left fw-700 features-bottom-text">
-                                    A high-quality link is worthless of Google doesn’t know about it
+                                    We check each URL linking to see if it’s indexed in Google.
                                 </p>
+
                             </div>
                         </div>
                     </div>
@@ -122,13 +141,12 @@
                         <div class="text-content">
                             <img src="/img/icons/features-icon-2.svg" alt="" title="" class="feature-icon">
                             <h3 class="pt-2 align-left">Canonical Check</h3>
-                            <p class="pt-3 align-left ">
-                                We make sure you know if the URL points to a different location than where you get your link from.
+                            <p class="pt-3 align-left ">Without canonical tags, search engines have a hard time scanning and indexing your site.
                             </p>
                             <div class="features-bottom-text-wrapper">
                                 <div class="list-icon"></div>
                                 <p class="pt-3 align-left fw-700 features-bottom-text">
-                                    Canonical tags are very important.
+                                    We ensure the URL points to a different location than where you get your link.
                                 </p>
                             </div>
                         </div>
@@ -140,12 +158,12 @@
                             <img src="/img/icons/features-icon-3.svg" alt="" title="" class="feature-icon">
                             <h3 class="pt-2 align-left">Client Rendering</h3>
                             <p class="pt-3 align-left ">
-                                We monitor if a site is rendered on the client side i.e. JavaScript
+                                Client-side rendered sites have no static HTML content and are notoriously hard to index by search engines.
                             </p>
                             <div class="features-bottom-text-wrapper">
                                 <div class="list-icon"></div>
                                 <p class="pt-3 align-left fw-700 features-bottom-text">
-                                    We inform you of links with more or less no value to you
+                                    With our system, you can check how it affects your backlinks.
                                 </p>
                             </div>
                         </div>
@@ -157,12 +175,12 @@
                             <img src="/img/icons/features-icon-4.svg" alt="" title="" class="feature-icon">
                             <h3 class="pt-2 align-left">Endpoint Check</h3>
                             <p class="pt-3 align-left ">
-                                We follow any 3xx redirects and show you the chain and endpoint.
+                                Redirecting pages ensures visitors don’t get lost and that changes to a website don’t have tons of dead ends.
                             </p>
                             <div class="features-bottom-text-wrapper">
                                 <div class="list-icon"></div>
                                 <p class="pt-3 align-left fw-700 features-bottom-text">
-                                    Redirects are vital to your links and less is more.
+                                    We follow any 3xx redirects and show you the chain and the endpoint.
                                 </p>
                             </div>
                         </div>
@@ -174,12 +192,12 @@
                             <img src="/img/icons/features-icon-5.svg" alt="" title="" class="feature-icon">
                             <h3 class="pt-2 align-left">HTML Headers</h3>
                             <p class="pt-3 align-left ">
-                                Monitors changes in HTML headers like nofollow, noindex, ugc and sponsored
+                                Undetected changes in HTML headers can distort the backlink connection.
                             </p>
                             <div class="features-bottom-text-wrapper">
                                 <div class="list-icon"></div>
                                 <p class="pt-3 align-left fw-700 features-bottom-text">
-                                    Make sure to get the most out of any link you get.
+                                    Traxr monitors changes in HTML headers like nofollow, noindex, UGC, and sponsored.
                                 </p>
                             </div>
                         </div>
@@ -189,52 +207,60 @@
                     <div class="features-box-2 pad-10">
                         <div class="text-content">
                             <img src="/img/icons/features-icon-6.svg" alt="" title="" class="feature-icon">
-                            <h3 class="pt-2 align-left">Response Headers</h3>
+                            <h3 class="pt-2 align-left">HTTP headers and status codes</h3>
                             <p class="pt-3 align-left ">
-                                As with HTML headers, which are visible in the to you, the Response headers are not.
+                                HTTP headers are essential for ensuring that your site has the correct prerequisites to make the backlink work.
                             </p>
                             <div class="features-bottom-text-wrapper">
                                 <div class="list-icon"></div>
                                 <p class="pt-3 align-left fw-700 features-bottom-text">
-                                    We Monitor for noindex and nofollow.
+                                    We monitor all HTTP status codes and HTTP headers.
                                 </p>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-4 padding-sm-mob">
-                    <div class="features-box-3 pad-10">
-                        <div class="text-content">
-                            <img src="/img/icons/features-icon-7.svg" alt="" title="" class="feature-icon">
-                            <h3 class="pt-2 align-left">Manual update</h3>
-                            <p class="pt-3 align-left ">
-                                We made it easy for you to update link status at any time you want
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4 padding-sm-mob">
-                    <div class="features-box-3 pad-10">
-                        <div class="text-content">
-                            <img src="/img/icons/features-icon-8.svg" alt="" title="" class="feature-icon">
-                            <h3 class="pt-2 align-left">Anchor text changes</h3>
-                            <p class="pt-3 align-left ">
-                                We monitor if the anchor text is changed for your link
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4 padding-sm-mob">
-                    <div class="features-box-3 pad-10">
-                        <div class="text-content">
-                            <img src="/img/icons/features-icon-9.svg" alt="" title="" class="feature-icon">
-                            <h3 class="pt-2 align-left">Number of domains & Links</h3>
-                            <p class="pt-3 align-left ">
-                                You can monitor multiple domains and links in the same package.
-                            </p>
-                        </div>
-                    </div>
-                </div>
+                <!--
+TODO missing last features, that should be at the top and be the most important here
+<div class="col-md-4 padding-sm-mob">
+    <div class="features-box-3 pad-10">
+        <div class="text-content">
+            <img src="/img/icons/features-icon-7.svg" alt="" title="" class="feature-icon">
+            <h3 class="pt-2 align-left">Manual asdasdasdasd</h3>
+            <p class="pt-3 align-left ">
+                We made it easy for you to update link status at any time you want
+            </p>
+        </div>
+    </div>
+</div>
+
+
+<div class="col-md-4 padding-sm-mob">
+    <div class="features-box-3 pad-10">
+        <div class="text-content">
+            <img src="/img/icons/features-icon-8.svg" alt="" title="" class="feature-icon">
+            <h3 class="pt-2 align-left">Anchor text changes</h3>
+            <p class="pt-3 align-left ">
+                We monitor if the anchor text is changed for your link
+            </p>
+        </div>
+    </div>
+</div>
+<div class="col-md-4 padding-sm-mob">
+    <div class="features-box-3 pad-10">
+        <div class="text-content">
+            <img src="/img/icons/features-icon-9.svg" alt="" title="" class="feature-icon">
+            <h3 class="pt-2 align-left">Number of domains & Links</h3>
+            <p class="pt-3 align-left ">
+                You can monitor multiple domains and links in the same package.
+            </p>
+        </div>
+    </div>
+</div>
+</div>
+--->
+            </div>
+        </div>
             </div>
         </div>
     </section>
